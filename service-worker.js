c95fa9009e10cdbf44941b71539732f5e7e383f4
nevergiveup980-ux <nevergiveup980@gmail.com>
2026-08-02 21:27:46 -0600

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE='runlu-invoice-build003-v1';
-const FILES=['./','index.html','styles.css','app.js','manifest.webmanifest','assets/icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
